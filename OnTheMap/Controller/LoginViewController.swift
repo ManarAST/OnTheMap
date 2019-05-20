61b9a360b506e0fe8c40dc09f4dc23639389b7c0
Manar <mnoooooor@gmail.com>
2019-05-20 23:54:28 +0300

    Done with most of the disgn
    I had alot of trial and error with the images

--- OnTheMap/Controller/LoginViewController.swift
+++ OnTheMap/Controller/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  OnTheMap
 //
 //  Created by manar AL-Towaim on 18/05/2019.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
